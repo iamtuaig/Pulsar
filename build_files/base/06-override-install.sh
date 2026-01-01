05ee57749f46b52f3403dee2b5709093cd2f987c
iamtuaig <sbwllrd@icloud.com>
2025-12-31 19:54:45 -0500

    Changes for improving build structure

---
+++ build_files/base/06-override-install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+echo "::group:: ===$(basename "$0")==="
+
+set -eoux pipefail
+
+# We do not need anything here at all
+rm -rf /usr/src
+rm -rf /usr/share/doc
+# Remove kernel-devel from rpmdb because all package files are removed from /usr/src
+rpm --erase --nodeps kernel-devel
+
+# Starship Shell Prompt
+ghcurl "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz" --retry 3 -o /tmp/starship.tar.gz
+tar -xzf /tmp/starship.tar.gz -C /tmp
+install -c -m 0755 /tmp/starship /usr/bin
+# shellcheck disable=SC2016
+echo 'eval "$(starship init bash)"' >>/etc/bashrc
+
+echo "::endgroup::"
